5def18d7e096aa70f66944db8a6369c89484665d
WoodNeck <saiyu915@naver.com>
2019-07-26 14:24:48 +0900

    :bug: quatToEuler => change rotation order to ZXY

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,4 +1,4 @@
-import { mat4, quat, vec3, glMatrix } from 'gl-matrix';
+import { mat4, quat, vec3 } from 'gl-matrix';
 import { BASE_ELEMENT_NOT_EXIST, MUST_STRING_OR_ELEMENT } from '../constants/error';
 import { Matrix4x4, Offset } from '../types';
 
@@ -91,42 +91,42 @@ export function radToDeg(rad: number): number {
   return 180 * rad / Math.PI;
 }
 
+// From Three.js https://github.com/mrdoob/three.js/blob/dev/src/math/Euler.js
 export function quatToEuler(q: quat): vec3 {
-  const x = q[0];
-  const y = q[1];
-  const z = q[2];
-  const w = q[3];
-  const x2 = x * x;
-  const y2 = y * y;
-  const z2 = z * z;
-  const w2 = w * w;
-  const unit = x2 + y2 + z2 + w2;
-  const test = x * w - y * z;
+  const rotM = mat4.create();
+  mat4.fromQuat(rotM, q);
 
-  let out = vec3.create();
-  if (test > glMatrix.EPSILON * unit) {
-    // singularity at the north pole
-    out[0] = Math.PI / 2;
-    out[1] = 2 * Math.atan2(y, x);
-    out[2] = 0;
-  } else if (test < -glMatrix.EPSILON * unit) {
-    // singularity at the south pole
-    out[0] = -Math.PI / 2;
-    out[1] = 2 * Math.atan2(y, x);
-    out[2] = 0;
+  const m11 = rotM[0];
+  const m12 = rotM[4];
+  // const m13 = rotM[8];
+  const m21 = rotM[1];
+  const m22 = rotM[5];
+  // const m23 = rotM[9];
+  const m31 = rotM[2];
+  const m32 = rotM[6];
+  const m33 = rotM[10];
+
+  const euler = vec3.create();
+
+  // ZXY
+  euler[0] = Math.asin(clamp(m32, -1, 1));
+  if (Math.abs(m32) < 0.99999) {
+    euler[1] = Math.atan2(-m31, m33);
+    euler[2] = Math.atan2(-m12, m22);
   } else {
-    out[0] = Math.atan2(2 * (x * w + y * z), 1 - 2 * (x2 + y2));
-    out[1] = Math.asin(2 * (w * y - x * z));
-    out[2] = Math.atan2(2 * (x * y + z * w), 1 - 2 * (y2 + z2));
+    euler[1] = 0;
+    euler[2] = Math.atan2(m21, m11);
   }
 
-  out = out.map(val => radToDeg(val)) as vec3;
-
-  return out;
+  return euler.map(val => radToDeg(val)) as vec3;
 }
 
 export function translateMat(mat: mat4, vec: vec3): void {
   mat[12] += vec[0];
   mat[13] += vec[1];
   mat[14] += vec[2];
+}
+
+export function clamp(val: number, min: number, max: number): number {
+  return Math.max(Math.min(val, max), min);
 }
