8294fdb64926d917f026b2d2b0707bf7487873db
WoodNeck <saiyu915@naver.com>
2019-08-09 19:23:04 +0900

    :lipstick: Filling homepage contents

--- demo/js/index.js
+++ demo/js/index.js
@@ -1,26 +1,28 @@
-const camera = new CSSCamera("#space");
-const windowWidth = document.body.offsetWidth;
-
-camera.perspective = windowWidth / 2;
-camera.position = [0, 0, windowWidth / 2];
-camera.update(0);
-
 const cardButton = document.querySelector("#card-button");
-const panelButton = document.querySelector("#panel-button");
+const codeButton = document.querySelector("#code-button");
 const heroButton = document.querySelector("#hero-button");
 const levelButton = document.querySelector("#level-button");
 
 const card = document.querySelector("#card");
-const panel = document.querySelector("#panel");
+const code = document.querySelector("#code");
 const hero = document.querySelector("#hero");
 const level = document.querySelector("#level");
+
+const camera = new CSSCamera("#space");
+const windowWidth = document.body.offsetWidth;
+
+camera.perspective = windowWidth / 2;
+camera.update(0);
+
+camera.focus(hero);
+camera.update(2000);
 
 cardButton.onclick = () => {
   camera.focus(card);
   camera.update(2000);
 }
-panelButton.onclick = () => {
-  camera.focus(panel);
+codeButton.onclick = () => {
+  camera.focus(code);
   camera.update(2000);
 }
 heroButton.onclick = () => {
