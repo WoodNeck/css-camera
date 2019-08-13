1c81ffb024956d7f838fc798a9066be467363a8f
WoodNeck <saiyu915@naver.com>
2019-08-13 11:51:22 +0900

    Updates

--- js/index.js
+++ js/index.js
@@ -1,33 +1,35 @@
+const cardButton = document.querySelector("#card-button");
+const codeButton = document.querySelector("#code-button");
+const heroButton = document.querySelector("#hero-button");
+const moreButton = document.querySelector("#more-button");
+
+const card = document.querySelector("#card");
+const code = document.querySelector("#code");
+const hero = document.querySelector("#hero");
+const more = document.querySelector("#more");
+
 const camera = new CSSCamera("#space");
 const windowWidth = document.body.offsetWidth;
 
 camera.perspective = windowWidth / 2;
-camera.position = [0, 0, windowWidth / 2];
 camera.update(0);
 
-const cardButton = document.querySelector("#card-button");
-const panelButton = document.querySelector("#panel-button");
-const heroButton = document.querySelector("#hero-button");
-const levelButton = document.querySelector("#level-button");
-
-const card = document.querySelector("#card");
-const panel = document.querySelector("#panel");
-const hero = document.querySelector("#hero");
-const level = document.querySelector("#level");
+camera.focus(hero);
+camera.update(2000);
 
 cardButton.onclick = () => {
   camera.focus(card);
   camera.update(2000);
 }
-panelButton.onclick = () => {
-  camera.focus(panel);
+codeButton.onclick = () => {
+  camera.focus(code);
   camera.update(2000);
 }
 heroButton.onclick = () => {
   camera.focus(hero);
   camera.update(2000);
 }
-levelButton.onclick = () => {
-  camera.focus(level);
+moreButton.onclick = () => {
+  camera.focus(more);
   camera.update(2000);
 }
