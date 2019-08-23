1802cf980dbd3fe2e72c81283e16e7555e59c3a8
WoodNeck <saiyu915@naver.com>
2019-08-23 18:31:19 +0900

    Updates

--- js/ortho.js
+++ js/ortho.js
@@ -1,9 +1,10 @@
-const windowWidth = document.body.offsetWidth;
-const windowHeight = document.body.offsetHeight;
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
+const center = document.querySelector("#center");
 
 const camera = new CSSCamera("#space");
 camera.viewportEl.style.backgroundColor = "white";
-camera.position = [-windowWidth * 3 / 8, -windowHeight * 3 / 8, 0];
+camera.focus(center);
 camera.update(0).then(async () => {
   await camera.update(1000);
 
@@ -14,64 +15,33 @@ camera.update(0).then(async () => {
   camera.rotation = [55, 0, -45];
   camera.scale = [2, 2, 2];
   camera.viewportEl.style.backgroundColor = "black";
-  camera.update(2000, {
+  await camera.update(2000, {
     property: "transform, background-color",
     timingFunction: "ease-out, linear",
     delay: "0ms, 0ms"
   });
+
+  var Axes = eg.Axes;
+  var PanInput = Axes.PanInput;
+  const axes = new Axes({
+    x: { range: [-400, 400] },
+    y: { range: [-150, 250] }
+  }, {
+    deceleration: 0.001
+  }, {
+    x: 0,
+    y: 0
+  });
+  const panInput = new PanInput(".cc-viewport", {
+    scale: [0.3, 0.3],
+  });
+
+  axes.on({
+    "change": evt => {
+      camera.translateLocal(-evt.delta.x, -evt.delta.y);
+      camera.update(0);
+    }
+  });
+
+  axes.connect(["x", "y"], panInput);
 });
-
-var up = false,
-right = false,
-down = false,
-left = false;
-
-document.addEventListener('keydown', press);
-function press(e){
-  if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */){
-    up = true;
-  }
-  if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */){
-    right = true;
-  }
-  if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */){
-    down = true;
-  }
-  if (e.keyCode === 37 /* left */ || e.keyCode === 65 /* a */){
-    left = true;
-  }
-}
-
-document.addEventListener('keyup', release);
-function release(e){
-  if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */){
-    up = false
-  }
-  if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */){
-    right = false
-  }
-  if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */){
-    down = false
-  }
-  if (e.keyCode === 37 /* left */ || e.keyCode === 65 /* a */){
-    left = false
-  }
-}
-
-const keyLoop = () => {
-  if (up){
-    camera.translateLocal(0, -5, 0);
-  }
-  if (right){
-    camera.translateLocal(5, 0, 0);
-  }
-  if (down){
-    camera.translateLocal(0, 5, 0);
-  }
-  if (left){
-    camera.translateLocal(-5, 0, 0);
-  }
-  camera.update(0);
-  requestAnimationFrame(keyLoop);
-}
-keyLoop();
