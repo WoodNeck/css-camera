53bb32cbe7802b90dec0d70d0928ae43bcfba1c6
WoodNeck <saiyu915@naver.com>
2019-09-09 19:07:00 +0900

    :camera_flash: Snapshot v1.0.0

--- lib/declaration/constants/error.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const ELEMENT_NOT_EXIST: (selector: string) => string;
-export declare const MUST_STRING_OR_ELEMENT: (received: any) => string;
--- lib/declaration/utils/math.d.ts
+++
@@ -1,4 +0,0 @@
-import { quat, vec3 } from 'gl-matrix';
-export declare function degToRad(deg: number): number;
-export declare function radToDeg(rad: number): number;
-export declare function quatToEuler(q: quat): vec3;
